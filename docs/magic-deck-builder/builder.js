2ec3ea76684daae8ad67d76ce80ea495f407392d
Alan Smith <commit@alanwsmith.com>
2026-03-20 22:30:15 -0400

    cleaning up data acquisition

--- docs/magic-deck-builder/builder.js
+++ docs/magic-deck-builder/builder.js
@@ -13,100 +13,45 @@ const colorKeys = {
 };
 
 export function buildUI() {
-  b.trigger("ui_colors ui_debuggingSwitch restoreState");
+  b.trigger("uiColors uiDebuggingSwitch restoreState");
 }
 
-export async function loadStateAndData() {
-  state = b.loadPage("state", { debugging: false });
-  console.log(state);
-  // Clear the state
-  // state = {};
-  // b.savePage("state", state);
+function filterCardName(card) {
+  const value = b.qs(`[data-r="displayNameSearch"]`).value.trim();
+  if (value === "") return true;
+  const pattern = new RegExp(value, "gi");
+  return card.name.match(pattern) !== null;
+}
 
-  b.debugging = state.debugging;
-  let leaders = ["a", "b", "c", "d", "e", "f"];
-  for (let i = 0; i <= 9; i += 1) leaders.push(`${i}`);
-  if (b.debugging) {
-    leaders = ["debug"];
-  }
-  for (let leader of leaders) {
-    const result = await b.get(`/magic-data/scryfall-cards/${leader}.json`);
-    if (result) {
-      result.cards.forEach((card) => {
-        allCards.push(card);
-      });
-    }
-  }
-  b.trigger("buildUI");
-  //b.trigger("init_colors init_nameSearch init_debuggingSwitch search");
-  // for (const [key, value] of Object.entries(state)) {
-  //   if (key.startsWith("value_")) {
-  //     b.qs(`[data-r="${key}"]`).value = value;
-  //   }
+export function filteredCards() {
+  return allCards
+    .filter((card) => filterCardName(card))
+    .filter((card, index) => index < 20);
+
+  // const output = allCards
+  //   .filter((card) => filterCardName(card));
+  // if (output.length === cards.length) {
+  //   el.innerHTML = "waiting";
+  //   b.send("tbd", "resultCount");
+  // } else {
+  //   b.send(output.length, "resultCount");
+  //   el.replaceChildren(
+  //     ...output
+  //       .sort((a, b) =>
+  //         a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+  //       )
+  //       .filter((card, index) => index < 20)
+  //       .map((card) => {
+  //         const subs = {
+  //           __CARD_NAME__: card.name,
+  //           __CARD_ID__: card.id,
+  //           __IMG_SRC__: card.image_uris.png,
+  //         };
+  //         return b.render("cardTemplate", subs);
+  //       }),
+  //   );
   // }
-}
-
-export function restoreState() {
-  setDisplayState(state.display);
-
-  //
-
-  // for (const [key, payload] of Object.entries(state)) {
-  //   if (key.startsWith("display_")) {
-  //     const el = b.qs(`[data-r="${key}"]`);
-  //     console.log(payload);
-  //     for (const dsKey in payload.dataset) {
-  //       el.setAttribute(`data-${dsKey}`, payload.dataset[dsKey]);
-  //     }
-  //     for (const ariaKey in payload.aria) {
-  //       el.setAttribute(`aria-${ariaKey}`, payload.aria[ariaKey]);
-  //     }
-  //     if (payload.value) {
-  //       el.value = payload.value;
-  //     }
-  //     if (payload.checked) {
-  //       el.checked = payload.checked;
-  //     }
-  //   }
-  // }
-
-  //
-}
-
-export function search(ev, __, ___) {
-  b.debounce("newSearch", "updateState", 200);
-}
-
-export function ui_colors(_, __, el) {
-  el.replaceChildren(
-    ...Object.keys(colorKeys).map((key) => {
-      return b.render("colorTemplate", {
-        __COLOR_KEY__: key,
-        __COLOR_NAME__: colorKeys[key].name,
-      });
-    }),
-  );
-}
-
-export function ui_debuggingSwitch(_, __, el) {
-  const subs = {
-    __APPEND__: "Debugging",
-    __RECEIVE__: "displayDebuggingToggle",
-    __SEND__: "updateDebuggingToggle",
-  };
-  el.replaceChildren(b.switch(subs));
-}
-
-export function updateDebuggingToggle(_, sender, ___) {
-  sender.toggleAria("checked");
-  updateState();
-}
-
-export function updateState() {
-  state.display = getDisplayState();
-  console.log(state);
-  b.savePage("state", state);
-  b.trigger("results");
+  // el.replaceChildren(output);
 }
 
 export function getDisplayState() {
@@ -138,6 +83,96 @@ export function getDisplayState() {
     }
     return item;
   });
+}
+
+export async function loadStateAndData() {
+  state = b.loadPage("state", { debugging: false });
+  b.debugging = state.debugging;
+  const hexChars = "abcdef1234567890";
+  let dataKeys = hexChars.split("");
+  if (b.debugging) dataKeys = ["debug"];
+  for (let dataKey of dataKeys) {
+    const result = await b.get(`/magic-data/scryfall-cards/${dataKey}.json`);
+    if (result) {
+      result.cards.forEach((card) => allCards.push(card));
+    }
+  }
+  b.trigger("buildUI");
+}
+
+export function restoreState() {
+  setDisplayState(state.display);
+  b.trigger("search");
+}
+
+export function results(_, __, el) {
+  el.replaceChildren(
+    ...filteredCards().map((card) => {
+      return b.render("cardTemplate");
+    }),
+  );
+
+  // const output = allCards
+  //   .filter((card) => filterCardName(card));
+  // if (output.length === cards.length) {
+  //   el.innerHTML = "waiting";
+  //   b.send("tbd", "resultCount");
+  // } else {
+  //   b.send(output.length, "resultCount");
+  //   el.replaceChildren(
+  //     ...output
+  //       .sort((a, b) =>
+  //         a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+  //       )
+  //       .filter((card, index) => index < 20)
+  //       .map((card) => {
+  //         const subs = {
+  //           __CARD_NAME__: card.name,
+  //           __CARD_ID__: card.id,
+  //           __IMG_SRC__: card.image_uris.png,
+  //         };
+  //         return b.render("cardTemplate", subs);
+  //       }),
+  //   );
+  // }
+  // el.replaceChildren(output);
+
+  //
+}
+
+export function search(ev, __, ___) {
+  b.debounce("newSearch", "updateState", 200);
+}
+
+export function uiColors(_, __, el) {
+  el.replaceChildren(
+    ...Object.keys(colorKeys).map((key) => {
+      return b.render("colorTemplate", {
+        __COLOR_KEY__: key,
+        __COLOR_NAME__: colorKeys[key].name,
+      });
+    }),
+  );
+}
+
+export function uiDebuggingSwitch(_, __, el) {
+  const subs = {
+    __APPEND__: "Debugging",
+    __RECEIVE__: "displayDebuggingToggle",
+    __SEND__: "updateDebuggingToggle",
+  };
+  el.replaceChildren(b.switch(subs));
+}
+
+export function updateDebuggingToggle(_, sender, ___) {
+  sender.toggleAria("checked");
+  updateState();
+}
+
+export function updateState() {
+  state.display = getDisplayState();
+  b.savePage("state", state);
+  b.trigger("results");
 }
 
 export function setDisplayState(els) {
