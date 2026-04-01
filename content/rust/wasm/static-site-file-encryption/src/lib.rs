d380155588b0e53f587813aef3345ba12e458cc5
Alan Smith <commit@alanwsmith.com>
2026-04-01 01:15:23 -0400

    hard coded password in place

--- content/rust/wasm/static-site-file-encryption/src/lib.rs
+++ content/rust/wasm/static-site-file-encryption/src/lib.rs
@@ -3,15 +3,31 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn encrypt_file(
-  cleartext_bytes: Vec<u8>,
+  bytes: Vec<u8>,
   password: String,
 ) -> Vec<u8> {
-  let user_password =
-    kdf::Password::from_slice(password.as_bytes()).unwrap();
+  let p2 = "wasm";
+  let kdf_password =
+    kdf::Password::from_slice(p2.as_bytes()).unwrap();
   let salt =
     kdf::Salt::from_slice(b"ca0beb1d-5a8a-4da9-b278-058087d00125")
       .unwrap();
-  let derived_key =
-    kdf::derive_key(&user_password, &salt, 3, 1 << 16, 32).unwrap();
-  aead::seal(&derived_key, &cleartext_bytes).unwrap()
+  let kdf_key =
+    kdf::derive_key(&kdf_password, &salt, 3, 1 << 16, 32).unwrap();
+  aead::seal(&kdf_key, &bytes).unwrap()
 }
+
+// #[wasm_bindgen]
+// pub fn encrypt_file(
+//   cleartext_bytes: Vec<u8>,
+//   password: String,
+// ) -> Vec<u8> {
+//   let user_password =
+//     kdf::Password::from_slice(password.as_bytes()).unwrap();
+//   let salt =
+//     kdf::Salt::from_slice(b"ca0beb1d-5a8a-4da9-b278-058087d00125")
+//       .unwrap();
+//   let derived_key =
+//     kdf::derive_key(&user_password, &salt, 3, 1 << 16, 32).unwrap();
+//   aead::seal(&derived_key, &cleartext_bytes).unwrap()
+// }
