cb45162ac4a73bd28f54d24b2fac917016581c5d
Alan Smith <commit@alanwsmith.com>
2026-04-02 19:32:02 -0400

    working on watcher

--- content/rust/al9000/src/watcher/mod.rs
+++ content/rust/al9000/src/watcher/mod.rs
@@ -1,7 +1,11 @@
 #![allow(unused)]
 use chrono::{DateTime, Local};
+use notify_debouncer_full::DebounceEventResult;
+use notify_debouncer_full::new_debouncer;
 use std::path::PathBuf;
 use tokio::sync::mpsc::Sender;
+use tokio::sync::mpsc::channel;
+use tokio::time::Duration;
 use tracing::info;
 
 pub struct Watcher {}
@@ -16,5 +20,15 @@ impl Watcher {
 
   pub async fn start(self) {
     info!("Starting Watcher");
+    let (watcher_internal_tx, mut watcher_internal_rx) =
+      channel::<chrono::DateTime<chrono::Local>>(1);
+    let mut debouncer = new_debouncer(
+      Duration::from_millis(200),
+      None,
+      move |result: DebounceEventResult| match result {
+        Ok(events) => {}
+        Err(e) => println!("{:?}", e),
+      },
+    );
   }
 }
