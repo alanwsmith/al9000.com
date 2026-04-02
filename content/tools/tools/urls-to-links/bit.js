9e8d8d1e30d9697421e2305f68708c7be08a44f7
Alan Smith <commit@alanwsmith.com>
2026-04-02 11:06:45 -0400

    updated URLs to Links

--- content/tools/tools/urls-to-links/bit.js
+++ content/tools/tools/urls-to-links/bit.js
@@ -13,3 +13,28 @@ export function urlsToLink(_, sender, el) {
   );
   el.value = output.join("\n");
 }
+
+export function clearURLsToLinks(_, __, el) {
+  el.value = "";
+  b.forwardSender(el, "updateURLsToLinks");
+}
+
+export function copyURLsToLinks(_, sender, el) {
+  b.quickCopy(el, sender);
+}
+
+export function showURLsToLinksExample(_, __, el) {
+  el.value = `https://www.example.com
+https://bittjs.com/`;
+  b.forwardSender(el, "updateURLsToLinks");
+}
+
+export function updateURLsToLinks(_, sender, el) {
+  const incoming = sender.value.split("\n").filter((line) => line.trim() !== "")
+    .sort();
+  b.dedupe(incoming);
+  const output = incoming.map((line) =>
+    `<li><a href="${line.trim()}">${line.trim()}</a></li>`
+  );
+  el.value = output.join("\n");
+}
