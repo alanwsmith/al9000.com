274a2e5b1cc21f8c98829f0ae0888ef39ff9b115
Alan Smith <commit@alanwsmith.com>
2026-04-02 16:09:43 -0400

    updated cc

---
+++ content/computercraft/local-scripts/startup.lua
@@ -0,0 +1,25 @@
+print("Running startup.lua")
+
+downloadFile = function(fileName)
+  print("Getting: " .. fileName)
+  local domain = "https://al9000.com"
+  local url = domain .. "/local-scripts/" .. fileName
+  local response = http.get(url)
+  if response then
+    local status = response.getResponseCode()
+    if status == 200 then
+      local output = fs.open(fileName, "w")
+      local data = response.readAll()
+      output.write(data)
+      output.close()
+    else
+      print("Non 200 response for: " .. fileName)
+    end
+  else
+    print("Unknown error for: " .. fileName)
+  end
+end
+
+downloadFile("menu.lua")
+shell.run("menu")
+
