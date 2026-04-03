b610dda5a0f6637943b1e223659726dd41f3158f
Alan Smith <commit@alanwsmith.com>
2026-04-03 12:16:11 -0400

    Working on signal based process restarter

--- content/rust/tokio/restart-process-when-signal-received/src/main.rs
+++ content/rust/tokio/restart-process-when-signal-received/src/main.rs
@@ -5,18 +5,49 @@ use tokio::time::{Duration, sleep};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+  //let mut second_process_handle: Option<JoinHandle<()>> = None;
+
+  let (tx, rx) = mpsc::channel::<usize>(100);
+  start_sender(tx)?;
+  start_listener(rx).await?;
+
+  // while let Some(count) = rx.recv().await {
+  //   if let Some(ref handle) = second_process_handle
+  //     && handle.is_finished()
+  //   {
+  //     second_process_handle = None;
+  //   }
+  //   if second_process_handle.is_none() {
+  //     second_process_handle = Some(tokio::spawn(async move {
+  //       secondary_process(count).await;
+  //     }));
+  //   } else {
+  //     second_process_handle.unwrap().abort();
+  //     second_process_handle = Some(tokio::spawn(async move {
+  //       secondary_process(count).await;
+  //     }));
+  //   }
+  // }
+
+  Ok(())
+}
+
+pub async fn secondary_process(count: usize) {
+  println!("{}: START", count);
+  tokio::time::sleep(tokio::time::Duration::from_millis(540)).await;
+  println!("{}: DONE", count);
+}
+
+pub async fn start_listener(
+  mut rx: mpsc::Receiver<usize>
+) -> Result<()> {
   let mut second_process_handle: Option<JoinHandle<()>> = None;
-
-  let (tx, mut rx) = mpsc::channel::<usize>(100);
-  start_sender(tx);
-
   while let Some(count) = rx.recv().await {
     if let Some(ref handle) = second_process_handle
       && handle.is_finished()
     {
       second_process_handle = None;
     }
-
     if second_process_handle.is_none() {
       second_process_handle = Some(tokio::spawn(async move {
         secondary_process(count).await;
@@ -28,17 +59,10 @@ async fn main() -> Result<()> {
       }));
     }
   }
-
   Ok(())
 }
 
-pub async fn secondary_process(count: usize) {
-  println!("{}: START", count);
-  tokio::time::sleep(tokio::time::Duration::from_millis(540)).await;
-  println!("{}: DONE", count);
-}
-
-pub fn start_sender(tx: mpsc::Sender<usize>) {
+pub fn start_sender(tx: mpsc::Sender<usize>) -> Result<()> {
   tokio::spawn(async move {
     for count in 0..11 {
       if tx.send(count).await.is_err() {
@@ -52,4 +76,5 @@ pub fn start_sender(tx: mpsc::Sender<usize>) {
       }
     }
   });
+  Ok(())
 }
