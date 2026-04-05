a4a2864fa91163059b7cedee09ef6fa2c7cc85f2
Alan Smith <commit@alanwsmith.com>
2026-04-05 15:49:01 -0400

    updated stem to only affect file names and not directories

--- content/rust/al9000.com/al9000/src/builder/functions/stem.rs
+++ content/rust/al9000.com/al9000/src/builder/functions/stem.rs
@@ -1,8 +1,16 @@
 use minijinja::Value;
 use std::path::PathBuf;
 
+// Reminder: This only strips extensions from
+// filenames. Directories are left as is so that
+// things like `templates.inc` don't get munged.
 pub fn stem(path: &str) -> Option<Value> {
   let pathbuf = PathBuf::from(path);
-  let file_stem = pathbuf.file_stem()?.display().to_string();
-  Some(Value::from_safe_string(file_stem))
+  if (pathbuf.is_file()) {
+    let file_stem = pathbuf.file_stem()?.display().to_string();
+    Some(Value::from_safe_string(file_stem))
+  } else {
+    let file_stem = pathbuf.file_name()?.display().to_string();
+    Some(Value::from_safe_string(file_stem))
+  }
 }
