2f6bc0179901de30e958bfbf7e7ff1eaf390f477
Alan Smith <commit@alanwsmith.com>
2026-04-06 15:16:08 -0400

    added open-graph data

--- content/rust/al9000.com/al9000/src/builder/functions/stem.rs
+++ content/rust/al9000.com/al9000/src/builder/functions/stem.rs
@@ -4,8 +4,12 @@ use std::path::PathBuf;
 // Reminder: This only strips extensions from
 // filenames. Directories are left as is so that
 // things like `templates.inc` don't get munged.
+// TODO: Move this into the main builder so
+// you can get the path to the content dir
+// automatically.
 pub fn stem(path: &str) -> Option<Value> {
-  let pathbuf = PathBuf::from(path);
+  let pathbuf =
+    PathBuf::from(format!("../../../../content{}", path));
   if (pathbuf.is_file()) {
     let file_stem = pathbuf.file_stem()?.display().to_string();
     Some(Value::from_safe_string(file_stem))
