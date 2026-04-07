d536c06c43b772f465b1f4146a42ad520125f42d
Alan Smith <commit@alanwsmith.com>
2026-04-07 09:44:09 -0400

    removed old wrappers

--- content/assembly/bit.js
+++ content/assembly/bit.js
@@ -1,54 +1,12 @@
-export const b = { init: "playerEls" };
+export const b = { init: "init" };
 
-let ytApiPromise;
-let players = [[], []];
-
-export async function playerEls(_, __, el) {
-  for (let i = 0; i < 4; i += 1) {
-    // const player = await addPlayer(el);
-    // players.push(player);
-    // seekAndPlay(i);
-  }
-}
-
-async function seekAndPlay(index) {
-  const times = b.data.timers.times;
-  const timer = times[b.randomInt(0, times.length - 1)];
-  players[index].seekTo(timer[0]);
-  await b.sleep(timer[1] * 1000);
-  seekAndPlay(index);
-}
-
-async function addPlayer(el) {
-  fetchApi();
-  await ytApiPromise;
-  const playerEl = b.ce("div");
-  el.append(playerEl);
-  return new Promise((resolve) => {
-    let player = new YT.Player(playerEl, {
-      width: "100%",
-      videoId: "kILYJ0qFhHM",
-      playerVars: { "playsinline": 1 },
-      events: {
-        "onReady": (event) => {
-          event.target.mute();
-          resolve(player);
-        },
-      },
+export async function init() {
+  [...b.qsa("video")].forEach((el) => {
+    el.addEventListener("ended", (event) => {
+      // event.target.src = "[@parent(file_path)@]/clips/assembly-_V1-0005.mp4";
+      // event.target.play();
+      console.log(event);
     });
   });
-}
-
-function fetchApi() {
-  if (window.YT || (window.YT && window.YT.Player)) return;
-  ytApiPromise = new Promise((res, rej) => {
-    var el = document.createElement("script");
-    el.src = "https://www.youtube.com/iframe_api";
-    el.async = true;
-    el.onload = (_) => {
-      YT.ready(res);
-    };
-    el.onerror = rej;
-    document.body.append(el);
-  });
+  b.qs("#v1").play();
 }
