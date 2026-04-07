241df601eec1d0516ac6ebd863ddc0b892923c2c
Alan Smith <commit@alanwsmith.com>
2026-04-07 18:07:32 -0400

    working on prose layout for assembly

--- content/assembly/bit.js
+++ content/assembly/bit.js
@@ -7,6 +7,21 @@ export async function init(_, __, el) {
     b.addStyles(`video { transform: rotate(0.01deg);}`);
   }
 
+  // Put a message up that this doesn't seem to
+  // work in safari.
+  if (
+    window.navigator.userAgent.includes("Safari") &&
+    !window.navigator.userAgent.includes("Chrome") &&
+    !window.navigator.userAgent.includes("Firefox")
+  ) {
+    console.log(window.navigator.userAgent);
+    el.innerHTML = `<p class="message">Unfortunately, this page doesn't work in 
+your browser. I'm not sure what's going on with it, but Safari won't 
+render it even though it works find in Chrome and Firefox. Give one of 
+    those a try if you want to see the page in action.</p>`;
+    return;
+  }
+
   const layout = getBlockLayout(
     el.propAsInt("minWidth"),
     el.propAsInt("maxWidth"),
@@ -15,12 +30,52 @@ export async function init(_, __, el) {
     el.propAsInt("ratioWidth"),
     el.propAsInt("ratioHeight"),
   );
-  for (let i = 0; i < layout.cells; i = i + 1) {
-    el.append(b.render("cellTemplate"));
-  }
   b.setCSS("--cell-width", `${layout.cellWidth}px`);
   b.setCSS("--cell-height", `${layout.cellHeight}px`);
-  b.trigger("initVideos");
+  // b.trigger("initVideos");
+
+  for (let layerIndex = 0; layerIndex <= 1; layerIndex = layerIndex + 1) {
+    const cells = [...new Array(layout.cells)].map((_, index) => {
+      return b.render("cellTemplate", {
+        __LAYER__: layerIndex,
+        __INDEX__: index,
+      });
+    });
+    el.appendChild(b.render("layerTemplate", {
+      __CELLS__: cells,
+    }));
+  }
+
+  b.qsa("video").forEach((el) => {
+    el.addEventListener("ended", (event) => {
+      const otherLayer = event.target.dataset.layer === "0" ? "1" : "0";
+      const index = event.target.dataset.index;
+      const switchTo = b.qs(
+        `video[data-layer='${otherLayer}'][data-index='${index}']`,
+      );
+      switchTo.classList.remove("hidden");
+      switchTo.play();
+      event.target.classList.add("hidden");
+      const vidIndex = b.randomInt(1, 9);
+      event.target.src =
+        `[@parent(file_path)@]/clips/assembly-_V1-000${vidIndex}.mp4`;
+      //console.log(otherLayer);
+      // event.target.src = "[@parent(file_path)@]/clips/assembly-_V1-0005.mp4";
+      // event.target.play();
+      // console.log(event);
+    });
+  });
+
+  b.qsa("video[data-layer='0']").forEach((el) => {
+    const index = b.randomInt(1, 9);
+    el.src = `[@parent(file_path)@]/clips/assembly-_V1-000${index}.mp4`;
+  });
+
+  b.qsa("video[data-layer='1']").forEach((el) => {
+    const index = b.randomInt(1, 9);
+    el.src = `[@parent(file_path)@]/clips/assembly-_V1-000${index}.mp4`;
+    el.play();
+  });
 
   //   el.addEventListener("ended", (event) => {
   //     // event.target.src = "[@parent(file_path)@]/clips/assembly-_V1-0005.mp4";
@@ -32,13 +87,13 @@ export async function init(_, __, el) {
   // b.qs("#v1").play();
 }
 
-export function initVideos() {
-  [...b.qsa("video")].forEach((el) => {
-    const index = b.randomInt(1, 9);
-    el.src = `[@parent(file_path)@]/clips/assembly-_V1-000${index}.mp4`;
-    el.play();
-  });
-}
+// export function initVideos() {
+//   [...b.qsa("video")].forEach((el) => {
+//     const index = b.randomInt(1, 9);
+//     el.src = `[@parent(file_path)@]/clips/assembly-_V1-000${index}.mp4`;
+//     el.play();
+//   });
+// }
 
 function getBlockLayout(
   minWidth,
