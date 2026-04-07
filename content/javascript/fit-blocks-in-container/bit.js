ad84f5f87e04139b035cadb814427cc1c788c4dd
Alan Smith <commit@alanwsmith.com>
2026-04-07 15:34:59 -0400

    moved spacer into function more directly

--- content/javascript/fit-blocks-in-container/bit.js
+++ content/javascript/fit-blocks-in-container/bit.js
@@ -1,11 +1,52 @@
-export const b = {
-  init: "blocks",
-};
+export const b = { init: "blocks" };
 
 export function blocks(_, __, el) {
+  const layout = getBlockLayout(
+    el.propAsInt("minWidth"),
+    el.propAsInt("maxWidth"),
+    parseInt(el.getBoundingClientRect().width),
+    parseInt(el.getBoundingClientRect().height),
+    el.propAsInt("ratioWidth"),
+    el.propAsInt("ratioHeight"),
+  );
+
+  for (let i = 0; i < layout.cells; i = i + 1) {
+    el.append(b.render("cellTemplate"));
+  }
+  b.setCSS("--cell-width", `${layout.cellWidth}px`);
+  b.setCSS("--cell-height", `${layout.cellHeight}px`);
+}
+
+function getBlockLayout(
+  minWidth,
+  maxWidth,
+  containerWidth,
+  containerHeight,
+  ratioWidth,
+  ratioHeight,
+) {
+  const maxItems = Math.floor(containerWidth / minWidth);
+  let cellWidth = Math.floor(containerWidth / maxItems);
+  if (cellWidth > maxWidth) {
+    cellWidth = maxWidth;
+  }
+  const cellHeight = cellWidth / ratioWidth * ratioHeight;
+  const columns = parseInt(containerWidth / cellWidth, 10);
+  const rows = parseInt(containerHeight / cellHeight, 10);
+  const cells = columns * rows;
+  return {
+    cellWidth: cellWidth,
+    cellHeight: cellHeight,
+    columns: columns,
+    rows: rows,
+    cells: cells,
+  };
+}
+
+export function _origina_blocks(_, __, el) {
   const elWidth = parseInt(el.getBoundingClientRect().width);
   const elHeight = parseInt(el.getBoundingClientRect().height);
-  const itemWidth = getItemWidth(
+  const itemWidth = getBlockLayout(
     el.propAsInt("minWidth"),
     el.propAsInt("maxWidth"),
     elWidth,
@@ -22,7 +63,7 @@ export function blocks(_, __, el) {
   b.setCSS("--cell-height", `${itemHeight}px`);
 }
 
-function getItemWidth(minWidth, maxWidth, containerWidth) {
+function _original_getBlockLayout(minWidth, maxWidth, containerWidth) {
   const maxItems = Math.floor(containerWidth / minWidth);
   const optimalWidth = Math.floor(containerWidth / maxItems);
   if (optimalWidth <= maxWidth) {
