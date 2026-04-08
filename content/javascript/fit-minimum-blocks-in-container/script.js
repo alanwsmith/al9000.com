bd57a7f13a51b0ba6dc6e6ec98560bc4299ab36f
Alan Smith <commit@alanwsmith.com>
2026-04-08 16:20:04 -0400

    finished min size container fill

--- content/javascript/fit-minimum-blocks-in-container/script.js
+++ content/javascript/fit-minimum-blocks-in-container/script.js
@@ -1,15 +1,14 @@
 function getLayout(d) {
   let cellWidth = Math.floor(d.containerWidth / d.minColumns);
-  console.log(d);
   for (let columns = d.minColumns + 2; columns <= d.maxColumns; columns += 2) {
     const checkWidth = Math.floor(d.containerWidth / columns);
-    console.log(checkWidth, d.maxWidth);
-
     if (checkWidth > d.maxWidth) {
       cellWidth = checkWidth;
+    } else {
+      break;
     }
   }
-  const cellHeight = parseInt(cellWidth / d.ratioWidth * d.ratioHeight);
+  const cellHeight = cellWidth / d.ratioWidth * d.ratioHeight;
   const columns = parseInt(d.containerWidth / cellWidth, 10);
   const rows = parseInt(d.containerHeight / cellHeight, 10);
   const cells = columns * rows;
