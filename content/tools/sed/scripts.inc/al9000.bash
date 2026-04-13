51d581cc63f0653817262b6d8fbd72efbd87af49
Alan Smith <commit@alanwsmith.com>
2026-04-13 13:05:01 -0400

    added sed and moved `content/tools/tools/*` to `content/tools/*`

---
+++ content/tools/sed/scripts.inc/al9000.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=~/workshop/al9000.com/content/tools
+
+# Reminder: -E is for extended RegEx
+# Reminder: -i is for editing the file in place
+
+find "$DIR" \
+  -type f \
+  \( \
+    -iname "*.html" \
+    -o -iname "*.js" \
+    -o -iname "*.txt" \
+    -o -iname "*.json" \
+  \) \
+  -print0 \
+| xargs -0 sed -E -i "" \
+'s!tools/tools!tools!g'
+
