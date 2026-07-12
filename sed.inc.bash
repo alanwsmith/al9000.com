3c1705c75d03fc6687b98dfe598b4e4742317015
Alan Smith <commit@alanwsmith.com>
2026-07-12 19:20:07 -0400

    updates

--- sed.inc.bash
+++ sed.inc.bash
@@ -8,5 +8,5 @@
 
 find "/Users/alan/workshop/al9000.com/content/" \
   -type f \( -iname "*.html" -o -iname "*.txt" -o -iname "*.md" -o -iname "*.neo" \) -print0 | xargs -0 sed -E -i "" \
-'s/highlight_block/highlight/g'
+'s/output_block/output/g'
 
