0b408ef7912da0342ff619504a4e37684a2bfc7c
Alan Smith <commit@alanwsmith.com>
2026-07-14 16:26:07 -0400

    moved to using p instad of f for file data

--- sed.inc.bash
+++ sed.inc.bash
@@ -8,5 +8,5 @@
 
 find "/Users/alan/workshop/al9000.com/content/" \
   -type f \( -iname "*.html" -o -iname "*.txt" -o -iname "*.md" -o -iname "*.neo" \) -print0 | xargs -0 sed -E -i "" \
-'s/highlight_command/command/g'
+'s/f.dir/p.dir/g'
 
