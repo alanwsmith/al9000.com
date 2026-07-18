5884e6689069b23858a9b3131e30ce3cd83ab58e
Alan Smith <commit@alanwsmith.com>
2026-07-18 01:01:26 -0400

    working on rss feed

--- sed.inc.bash
+++ sed.inc.bash
@@ -8,6 +8,6 @@
 
 find "/Users/alan/workshop/al9000.com/content/" \
   -type f \( -iname "*.html" -o -iname "*.txt" -o -iname "*.md" -o -iname "*.neo" \) -print0 | xargs -0 sed -E -i "" \
-  's|== extends "/templates.inc/pages/empty.html"|== if rss\n== extends "/templates.inc/rss/empty.html"\n== else\n== extends "/templates.inc/pages/empty.html"\n== endif|g'
+  's|== extends "/templates.inc/pages/rust.html"|== if rss\n== extends "/templates.inc/rss/default.html"\n== else\n== extends "/templates.inc/pages/default.html"\n== endif|g'
 
 
