aa702fc8c20a5e9559d308dfb20a59c3b47549f5
Alan Smith <commit@alanwsmith.com>
2026-07-20 13:50:49 -0400

    working on id update

--- _site/rust/apps/base32_timestamp_cleanup/src/main.rs
+++ _site/rust/apps/base32_timestamp_cleanup/src/main.rs
@@ -1,11 +1,44 @@
-use std::path::PathBuf;
+use anyhow::Result;
+use base32::Alphabet;
+use chrono::{DateTime, FixedOffset};
+use regex::Regex;
+use std::{fs, path::PathBuf};
 use walkdir::WalkDir;
 
-fn main() {
+fn main() -> Result<()> {
   let files = get_files_with_extensions(
     &PathBuf::from("../../.."),
     &vec!["html"],
   );
+  let date_matcher = Regex::new(
+    r"created\s+=\s+(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d-\d\d:\d\d)",
+  )?;
+  for f in files {
+    let content = fs::read_to_string(f)?;
+    for (_, [date]) in
+      date_matcher.captures_iter(&content).map(|c| c.extract())
+    {
+      let dt = DateTime::parse_from_rfc3339(date)?;
+      let encoded = encode_base32_datetime(&dt);
+      let id = format!(
+        "{}/{}/{}/{}",
+        &encoded[0..=1],
+        &encoded[2..=3],
+        &encoded[4..=5],
+        &encoded[6..=7],
+      );
+      dbg!(id);
+    }
+  }
+
+  Ok(())
+}
+
+fn encode_base32_datetime(
+  datetime: &DateTime<FixedOffset>
+) -> String {
+  let bytes = &datetime.timestamp().to_be_bytes()[3..8];
+  base32::encode(Alphabet::Crockford, bytes).to_lowercase()
 }
 
 fn get_files_with_extensions(
