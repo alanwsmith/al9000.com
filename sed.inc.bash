5d5f8f7838a19ed1163579bc43a40ab80adad0b2
Alan Smith <commit@alanwsmith.com>
2026-07-24 09:55:41 -0400

    working on color picker

--- sed.inc.bash
+++ sed.inc.bash
@@ -7,6 +7,6 @@
 
 find "/Users/alan/workshop/al9000.com/content/" \
   -type f \( -iname "*.js" -o -iname "*.html" -o -iname "*.txt" -o -iname "*.md" -o -iname "*.neo" \) -print0 | xargs -0 sed -E -i "" \
-  's|getData\(|getJSON(|g'
+  's|template_folder|template_dir|g'
 
 
