c9bfc9dcffe429510314894286f1cb55a1efe9c2
Alan Smith <commit@alanwsmith.com>
2026-07-28 15:48:06 -0500

    set up select for blocks

--- _site/color-picker/bit.js
+++ _site/color-picker/bit.js
@@ -22,55 +22,16 @@ export function backgroundSlidersInit(_, __, el) {
   b.trigger("backgroundSliderUpdate");
 }
 
-export function blockOptionsInit(_, __, el) {
-  // b.trace("blockOptionsInit");
-  // const mode = s.getActiveMode();
-  // const options = Object.keys(mode.blocks).map((key) => {
-  //   let selected = "";
-  //   if (key === mode.activeBlock) {
-  //     selected = " selected";
-  //   }
-  //   const optionSubs = {
-  //     __KEY__: key,
-  //     __SELECTED__: selected,
-  //   };
-  //   return b.render("blockOption", optionSubs);
-  // });
-  // const subs = {
-  //   __OPTIONS__: options,
-  // };
-  // el.replaceChildren(b.render("blockOptions", subs));
-}
-
-// export function blockOptionsSet(_, sender, ___) {
-//   const mode = s.getActiveMode();
-//   b.info(mode.blocks);
-// }
-
-// export function blockOptionsUpdate(_, __, el) {
-//   el.replaceChildren();
-//   const mode = s.getActiveMode();
-//   s.data.colorTypes.map((colorType) => {
-//     return s.data.colorNames.map((colorName) => {
-//       const key = `${colorType}-${colorName}`;
-//       let selected = "";
-//       if (mode.activeBlock === key) {
-//         selected = " selected";
-//       }
-//       const subs = {
-//         __KEY__: `${colorType}-${colorName}`,
-//         __SELECTED__: selected,
-//       };
-//       el.appendChild(b.render("blockBuilderOption", subs));
-//     });
-//   });
-// }
-
 export function blockSelectInit(_, __, el) {
   b.trace("blockSelectInit");
-  s.data.colorKeys.forEach((key) => {
-    const subs = {};
-    el.appendChild(b.render("blockOption", subs));
+  s.data.colorKeys.forEach((color) => {
+    s.data.colorTypes.forEach((type) => {
+      const subs = {
+        __KEY__: `${type}-${color}`,
+        __NAME__: `${color}: ${type}`,
+      };
+      el.appendChild(b.render("blockOption", subs));
+    });
   });
 
   // el.replaceChildren(b.render("blockOption"));
--- content/color-picker/components.inc/blockSelect.js
+++ content/color-picker/components.inc/blockSelect.js
@@ -1,8 +1,13 @@
 export function blockSelectInit(_, __, el) {
   b.trace("blockSelectInit");
-  s.data.colorKeys.forEach((key) => {
-    const subs = {};
-    el.appendChild(b.render("blockOption", subs));
+  s.data.colorKeys.forEach((color) => {
+    s.data.colorTypes.forEach((type) => {
+      const subs = {
+        __KEY__: `${type}-${color}`,
+        __NAME__: `${color}: ${type}`,
+      };
+      el.appendChild(b.render("blockOption", subs));
+    });
   });
 
   // el.replaceChildren(b.render("blockOption"));
